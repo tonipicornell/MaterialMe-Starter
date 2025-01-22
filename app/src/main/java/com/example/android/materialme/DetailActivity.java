48e907e8a23c4421b40935e324825594f1f13e99
tonipicornell <tpicornell59@gmail.com>
2025-01-22 13:36:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/materialme/DetailActivity.java
@@ -0,0 +1,35 @@
+package com.example.android.materialme;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_detail);
+
+        TextView sportsTitle = (TextView)findViewById(R.id.titleDetail);
+        ImageView sportsImage = (ImageView)findViewById(R.id.sportsImageDetail);
+
+        sportsTitle.setText(getIntent().getStringExtra("title"));
+        Glide.with(this).load(getIntent().getIntExtra("image_resource", 0)).into(sportsImage);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
